63a3939755eca5de379f0112119b0db49c946d45
IsiraViduranga <isira.20211287@iit.ac.lk>
2023-03-15 02:19:09 +0530

    Setting up Firebase

--- lib/utils/main.dart
+++ lib/utils/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../screens/login.dart';
+import '../screens/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const VisionaryRX());
 }
 
